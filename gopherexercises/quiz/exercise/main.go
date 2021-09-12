76be3910125fc49e2b8743715c499976ed1d78d5
davidlzs <davidlzs@gmail.com>
2021-09-12 17:18:19 -0600

    Add timer to quiz

--- gopherexercises/quiz/exercise/main.go
+++ gopherexercises/quiz/exercise/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 type quizItem struct {
@@ -30,19 +31,53 @@ func main() {
 		panic(err)
 	}
 
+	timer := time.NewTimer(30 * time.Second)
+
+	ch := make(chan string)
+	go run(ch, questions)
+
+	for {
+		select {
+		case <-timer.C:
+			ch <- "TimeOut"
+		case msg := <-ch:
+			if strings.Compare("Done", msg) == 0 {
+				fmt.Println("Done")
+				os.Exit(0)
+			} else if strings.Compare("TimeOut", msg) == 0 {
+				fmt.Println("Time Out")
+				os.Exit(1)
+			}
+		}
+	}
+}
+
+func run(ch chan string, questions []quizItem) {
 	var correctCount int
 	reader := bufio.NewReader(os.Stdin)
 	for _, question := range questions {
+		stdinCh := make(chan string)
+		go func(ch chan string) {
+			text, _ := reader.ReadString('\n')
+			ch <- text
+		}(stdinCh)
+
 		fmt.Printf("Question#%s %s=", question.index, question.question)
-		text, _ := reader.ReadString('\n')
-		if strings.Compare(question.answer, strings.TrimSuffix(text, "\n")) == 0 {
-			correctCount++
-		} else {
+
+		select {
+		case <-ch:
+			fmt.Printf("\nYou scored %d out of %d\n", correctCount, len(questions))
+			ch <- "TimeOut"
+			return
+		case answer := <-stdinCh:
+			if strings.Compare(question.answer, strings.TrimSuffix(answer, "\n")) == 0 {
+				correctCount++
+			}
 		}
 	}
 
 	fmt.Printf("You scored %d out of %d\n", correctCount, len(questions))
-
+	ch <- "Done"
 }
 
 func loadQuiz(filePath string) ([]quizItem, error) {
